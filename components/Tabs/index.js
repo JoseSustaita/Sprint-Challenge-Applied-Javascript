037031c7867489da540c1c4d23d72924e8b1d565
Jose Sustaita <josesustaita70@gmail.com>
2020-02-23 20:16:18 -0500

    Completed Tabs
    Used axios to get request. Made new tab component by iterrating over topics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,19 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(response => {
+
+
+    topicsDiv = document.querySelector(".topics")
+    topicsArray = response.data.topics;
+    topicsArray.forEach(topic => {
+        let tabs = document.createElement("div")
+        tabs.classList.add("tab")
+        tabs.textContent = topic;
+        topicsDiv.appendChild(tabs)
+    });
+    
+  });
